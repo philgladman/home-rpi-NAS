30b9066dfe8dc0191f9580885351dace603691e2
Phillip Gladman <Philgladman@Phillps-MacBook-Pro.local>
2022-11-12 22:50:12 -0500

    removing test values

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,7 @@
 /etc/init.d/smbd restart
 
-export USER=phil
-
-export PASSWORD=testpass
-
 useradd "$USER" -p "$PASSWORD" --no-create-home --shell /usr/sbin/nologin
 
 (echo "$PASSWORD"; echo "$PASSWORD") | smbpasswd -s -a "$USER"
 
-#/bin/bash
 sleep infinity
